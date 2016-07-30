e90d4f3c7f3ca2ab849f0d57fc4272c18167f1dd
Benjamin Huang <y.x.ben.h@gmail.com>
2016-07-30 16:35:19 +1000

    proto index and code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function loadGame() {
-    var viewSizeX = 800;
-    var viewSizeY = 600;
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+    var viewSizeX = 1000;
+    var viewSizeY = 100;
+    var game = new Phaser.Game(viewSizeX, viewSizeY, Phaser.AUTO, 'phaserCanvas',
+                                { preload: preload, create: create, update: update });
+    var character;
     var uiTextFormats = { font: "25px Arial", fill: '#FFFFFF' };
     var textAgeLabel, textAgeValue, textGameStateLabel, textGameStateValue;
 
@@ -11,20 +13,29 @@ function loadGame() {
 
     function create() {
         // setup player character animation loading.
-        character = game.add.sprite(400, 450, "playerCharacter"); // 
-        character.animations.add('walking', [32, 33, 34, 35, 36, 37, 38, 39], 10, true);
+        character = game.add.sprite(0, viewSizeY/1.1, "playerCharacter"); // 
+        character.animations.add('walking', [32, 33, 34, 35, 36, 37, 38, 39], 8, true);
         character.animations.add('death', [18, 19, 20, 21, 22, 23], 10, true);
         character.animations.add('standing', [63], 10, false);
         character.anchor.setTo(0.5, 1);
-        character.scale.setTo(3.0, 3.0);
+        character.scale.setTo(1.0, 1.0);
 
         // setup common UI text
-        textAgeLabel = game.add.text(0, 0, 'Age: ', uiTextFormats);
-        textAgeValue = game.add.text(70, 0, '{?}', uiTextFormats);
-        textGameStateLabel = game.add.text(viewSizeX-400, 0, 'Current State: ', uiTextFormats);
-        textGameStateValue = game.add.text(viewSizeX-230, 0, '{?}', uiTextFormats);
+        // textAgeLabel = game.add.text(0, 0, 'Age: ', uiTextFormats);
+        // textAgeValue = game.add.text(70, 0, '{?}', uiTextFormats);
+        // textGameStateLabel = game.add.text(viewSizeX-400, 0, 'Current State: ', uiTextFormats);
+        // textGameStateValue = game.add.text(viewSizeX-230, 0, '{?}', uiTextFormats);
 
     }
+
+    var GameStateEnum = {
+        InitialPlayerSetup:     0,
+		AnimateLifeProgress:    1,
+		CalculateLifeAndReport: 2,
+		LifeEnded:              3
+    };
+
+    var gameState = GameStateEnum.InitialPlayerSetup;
 
     function update() {
         character.animations.play("walking");
